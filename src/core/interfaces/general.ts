67aaad8e7012b27c265769bc3e724649c977c35d
Mykhailo <mykhaylohud@gmail.com>
2025-12-11 16:46:25 +0200

    feat: fixed typeCheck

--- src/core/interfaces/general.ts
+++
@@ -1,5 +0,0 @@
-import { ApiError } from "@app/core/services";
-import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
-
-// Legacy RTK Query error type
-export type QueryError = FetchBaseQueryError & { data?: ApiError };
